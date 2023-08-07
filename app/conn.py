a4c179c6dbfcc33eafb968e7685905c86a32f7e0
vinayvnu <vinay.t1112@gmail.com>
2023-08-07 23:03:21 +0530

    2nd eod commit

--- app/conn.py
+++ app/conn.py
@@ -9,23 +9,19 @@ db_config = {
     "database": "BlazerexSql",
 }
 
-def db_conn():
+
+def db_conn_select(sql_to_execute):
     cursor = None
     connection = None
     try:
-        # Connect to the database
         connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(sql_to_execute)
 
-        # Create a cursor
-        cursor = connection.cursor()
-
-        # Execute a sample query
-        # cursor.execute("SELECT * FROM apicon.posts")
-
-        # Fetch and print the results
-        # rows = cursor.fetchall()
+        rows = cursor.fetchall()
         # for row in rows:
         #     print(row)
+        return rows
 
     except mysql.connector.Error as err:
         print("Error:", err)
@@ -38,8 +34,97 @@ def db_conn():
         if connection:
             connection.close()
 
-    return cursor
+
+def db_conn_insert(sql_to_execute):
+    cursor = None
+    connection = None
+    try:
+        print("db_conn_insert1")
+        connection = mysql.connector.connect(**db_config)
+        print("db_conn_insert1.1")
+        cursor = connection.cursor()
+        print("db_conn_insert1.2")
+        print(sql_to_execute)
+        cursor.execute(sql_to_execute)
+        print("db_conn_insert2")
+
+        rows = cursor.fetchone()
+        connection.commit()
+        print("db_conn_insert3")
+
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+        print("db_conn_insert4")
+        return rows
+
+    except mysql.connector.Error as err:
+        print("Error:", err)
+        return None
+
+    finally:
+        # Close the cursor and connection
+        print("finally called --------------")
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
 
 
+def db_conn_delete(sql_to_execute):
+    cursor = None
+    connection = None
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        cursor.execute(sql_to_execute)
+
+        connection.commit()
+
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+        return None
+
+    except mysql.connector.Error as err:
+        print("Error:", err)
+        return None
+
+    finally:
+        # Close the cursor and connection
+        print("finally called delete--------------")
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
 
 
+def db_conn_update(sql_to_execute):
+    cursor = None
+    connection = None
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        cursor.execute(sql_to_execute)
+
+        connection.commit()
+
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+        return None
+
+    except mysql.connector.Error as err:
+        print("Error:", err)
+        return None
+
+    finally:
+        # Close the cursor and connection
+        print("finally called update--------------")
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Response, status, HTTPException
 # from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+from app.conn import db_conn_select, db_conn_insert, db_conn_delete, db_conn_update
 
 app = FastAPI()
 
@@ -13,7 +14,6 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
-
 
 
 my_posts = [
@@ -42,23 +42,37 @@ async def root():
 @app.get("/posts")
 def get_posts():
     print("here5")
+    data = db_conn_select("""select * from apicon.posts order by id""")
+    print(data)
     return {"data": my_posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    print("here4")
-    # print(post.model_dump())
-    post_dict = post.model_dump()
-    post_dict["id"] = randrange(0, 1000000)
-    my_posts.append(post.model_dump())
-    return {"data": post_dict}
+    # print("here4")
+    # # print(post.model_dump())
+    # post_dict = post.model_dump()
+    # post_dict["id"] = randrange(0, 1000000)
+    # my_posts.append(post.model_dump())
+    publish_value_pass = 1
+    if post.published:
+        publish_value_pass = 1
+    else:
+        publish_value_pass = 0
+    query = f"INSERT INTO `apicon`.`posts` (`title`, `content`, `PUBLISHED`) VALUES ('{post.title}', '{post.content}', {publish_value_pass})"
+    print(query)
+
+    returned_data = db_conn_insert(query)
+    print(f"returned_data: {returned_data}")
+    return {"data"}
 
 
 @app.get("/posts/{id}")
 def get_post(id_passed: int):
     print("here3")
-    post = find_post(id_passed)
+    post = db_conn_select(f"select * from apicon.posts where id={id_passed}")
+    print(post)
+    # post = find_post(id_passed)
     if not post:
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'message': f"post with id: {id_passed} was not found"}
@@ -73,14 +87,20 @@ def get_post(id_passed: int):
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id_passed: int):
     print("here1")
-    index = find_index_post(id_passed)
+    # index = find_index_post(id_passed)
+    index = db_conn_select(f"select * from apicon.posts where id={id_passed}")
+    print(index)
+
     if index is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id_passed} does not exist",
         )
 
-    my_posts.pop(index)
+    print("here2")
+
+    db_conn_delete(f"delete from apicon.posts where id={id_passed}")
+    # my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
@@ -88,14 +108,25 @@ def delete_post(id_passed: int):
 def update_post(id_to_update: int, post: Post):
     print("here2")
     print(post)
-    index = find_index_post(id_to_update)
+    # index = find_index_post(id_to_update)
+    index = db_conn_select(f"select * from apicon.posts where id={id_to_update}")
     if index is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id_to_update} does not exist",
         )
 
-    post_dict = post.model_dump()
-    post_dict["id"] = id_to_update
-    my_posts[index] = post_dict
+    publish_value = 1
+    if post.published:
+        publish_value = 1
+    else:
+        publish_value = 0
+
+    db_conn_update(
+        f"update apicon.posts set title= '{post.title}', content= '{post.content}', published={publish_value} where id={id_to_update}"
+    )
+    # post_dict = post.model_dump()
+    # db_conn_update
+    # post_dict["id"] = id_to_update
+    # my_posts[index] = post_dict
     return {"message": f"updated post {id_to_update}"}
