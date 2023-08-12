94ee19dca9b7a0198a96c8b3ea6e700071a04517
vinayvnu <vinay.t1112@gmail.com>
2023-08-12 22:57:28 +0530

    CORS functionality added

--- app/main.py
+++ app/main.py
@@ -2,10 +2,22 @@ from fastapi import FastAPI
 from .sqlalchemy import engine
 from . import model
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 
 model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+# this is the list who are allowed to access the api
+origins = ["https://www.google.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 app.include_router(post.router)
